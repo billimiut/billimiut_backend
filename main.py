4767d764ff20b561e26371acc943d5935c8a3b7d
Jiyeon Park <helmrping@gmail.com>
2024-02-10 23:14:12 +0900

    The nickname in the Post was changed to the writer_id.

--- main.py
+++ main.py
@@ -66,7 +66,7 @@ class GeoPoint(BaseModel):
 # category_id <- 일단 category: str으로 대체
 class Post(BaseModel):
     post_id: str = ""
-    nickname: str
+    writer_id: str = ""
     title: str
     item: str
     category: str # 변경
@@ -249,7 +249,7 @@ async def read_posts():
     result = []
     for doc in docs:
         data = doc.to_dict()
-        selected_fields = {field: data.get(field, None) for field in ['post_id', 'nickname', 'title', 'description', 'item', 'image_url', 'money', 'borrow', 'description', 'emergency', 'start_date', 'end_date', 'location_id', 'female', 'status', 'category_id', 'borrower_user_id', 'lender_user_id']}
+        selected_fields = {field: data.get(field, None) for field in ['post_id', 'writer_id', 'title', 'description', 'item', 'image_url', 'money', 'borrow', 'description', 'emergency', 'start_date', 'end_date', 'location_id', 'female', 'status', 'category_id', 'borrower_user_id', 'lender_user_id']}
         result.append(selected_fields)
     return result
 
@@ -284,6 +284,7 @@ async def add_post(data: Add_Post):
     
     try:
         post_dict = post.dict()
+        post_dict["writer_id"] = user_id
         post_dict["post_id"] = doc_ref1.id
         post_dict["location_id"] = doc_ref2.id
         doc_ref1.set(post_dict)
