b741cd54303352fb49948cfd68fded202133cf49
W00SHH <jerry7780@g.skku.edu>
2024-08-07 01:14:09 +0900

    add: 게시물 신고 기능 추가 (테스트 및 논의 필요)

--- app/controllers/post_controller.py
+++ app/controllers/post_controller.py
@@ -6,7 +6,7 @@ from fastapi import APIRouter, HTTPException, UploadFile, File, Form
 from pydantic import ValidationError
 
 from app.middlewares.images import upload_image
-from app.models.post_models import insert_post, find_post, find_posts, update_post_status, erase_post, find_posts_by_user, update_post, edit_post_image_url
+from app.models.post_models import insert_post, find_post, find_posts, update_post_status, erase_post, find_posts_by_user, update_post, edit_post_image_url, report_post
 from app.models.users_models import find_user_by_id
 from app.models.users_models import update_user_post
 from app.schemas.post_schema import PostBase, PostUpdate
@@ -186,3 +186,12 @@ async def put_post_by_post_id(post_id: str, post: str = Form(...), add_image: Li
     except Exception as e:
         print(e)
         return HTTPException(status_code=400, detail="Update post failed")
+
+@router.post("/post/report/{post_id}")
+async def post_report_post(post_id: str, reporter_uuid: str, report_reason: str):
+    try:
+        res = report_post(post_id, reporter_uuid, report_reason)
+        return res["message"] # 현재는 어떤 결과가 나왔다라고만 이렇게 리턴을 하는데, 이거는 추후 변경하는게 좋을 것 같음.
+    except Exception as e:
+        print(e)
+        return HTTPException(status_code=400, detail="Report post failed")
