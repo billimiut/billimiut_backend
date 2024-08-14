9b67f782cb4705a4389d3c22c4c86e51bc59c9b2
W00SHH <jerry7780@g.skku.edu>
2024-08-14 14:36:10 +0900

    add: status 상태변경 미들웨어 추가

---
+++ app/middlewares/status_updater.py
@@ -0,0 +1,17 @@
+from fastapi import Request
+import time
+from ..db.session import client
+from starlette.middleware.base import BaseHTTPMiddleware
+
+collection = 'post'
+
+class statusMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request: Request, call_next):
+        current_time = time.time()
+        collection = client['post']
+        collection.update_many(
+            {"end_date": {"$lt": current_time}, "status": {"$ne": "종료"}},
+            {"$set": {"status": "종료"}}
+        )
+        response = await call_next(request)
+        return response
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from app.controllers import users_controller, post_controller, chat_controller
 from app.middlewares.logger import LoggingMiddleware
+from app.middlewares.status_updater import statusMiddleware
+
 app = FastAPI()
 
 app.include_router(users_controller.router)
@@ -8,3 +10,4 @@ app.include_router(post_controller.router)
 app.include_router(chat_controller.router)
 
 app.add_middleware(LoggingMiddleware)
+app.add_middleware(statusMiddleware)
