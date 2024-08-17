02fbeb3e629528238fc08914cdb0c01a70add74e
JIGOOOD <helmrping@gmail.com>
2024-08-18 02:11:49 +0900

    test filtering

--- app/controllers/post_controller.py
+++ app/controllers/post_controller.py
@@ -193,3 +193,35 @@ async def post_report_post(post_id: str, report: PostReport):
     except Exception as e:
         print(e)
         return HTTPException(status_code=400, detail="Report post failed")
+
+@router.get("/post/filter/{filter}")
+async def filter_post(filter: str, res: List[PostBase]):
+    try:
+        filtered_posts = []
+        
+        for post in res:
+            if post['borrow']:
+                writer_uuid = post['borrower_uuid']
+            else:
+                writer_uuid = post['lender_uuid'] 
+            
+            writer_info, message = find_user_by_id(UserGetInfo(id=writer_uuid))
+            post['nickname'] = writer_info['nickname']
+            post['profile_image'] = writer_info['profile_image']
+            post['writer_uuid'] = writer_uuid
+            post_id = post['_id']
+            del post['_id']
+            post['post_id'] = post_id
+
+            if filter == "ing" and post['status'] == '게시': # 게시중 필터링
+                filtered_posts.append(post)
+        
+        if filter == "distance": # 거리순 필터링
+            filtered_posts = sorted(res, key=lambda x: x.get('distance', float('inf')))
+        
+        print(filtered_posts)
+        return filtered_posts
+    except Exception as e:
+        traceback.print_exc()
+        print(e)
+        raise HTTPException(status_code=400, detail="Filter posts failed")
