4fc503978045c56368cb1c4e84fb521cd0b38315
W00SHH <jerry7780@g.skku.edu>
2024-09-04 04:14:17 +0900

    fix: status_updater KST 시간으로 변환

--- app/middlewares/status_updater.py
+++ app/middlewares/status_updater.py
@@ -1,5 +1,5 @@
 from fastapi import Request
-from datetime import datetime
+from datetime import datetime, timedelta
 from ..db.session import client
 from starlette.middleware.base import BaseHTTPMiddleware
 
@@ -7,7 +7,7 @@ collection = 'post'
 
 class statusMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next):
-        current_time = datetime.utcnow()
+        current_time = datetime.now() + timedelta(hours=9)
         collection = client['post']
         collection.update_many(
             {"end_date": {"$lt": current_time}, "status": {"$ne": "종료"}},
