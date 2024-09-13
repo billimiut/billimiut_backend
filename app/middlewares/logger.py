36107c0556df12598c4e31cd2d8d37061fb30add
W00SHH <jerry7780@g.skku.edu>
2024-09-13 11:16:21 +0900

    fix: 오류로 인해 로그 임시 주석처리

--- app/middlewares/logger.py
+++ app/middlewares/logger.py
@@ -11,7 +11,6 @@ from ..utils.log_util import logger
 class LoggingMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next):
 
-        logger.info(f"{datetime.datetime.now()} Request {request.method} {request.url}")
         # body = await request.body()
         # if body:
         #     logger.info(f"{datetime.datetime.now()} Request {request.method} {request.url}, Body: {body.decode('utf-8')}")
@@ -29,5 +28,14 @@ class LoggingMiddleware(BaseHTTPMiddleware):
         # else:
         #     logger.info(f"{datetime.datetime.now()} Response to {request.method} {request.url} Status {response.status_code}")
         # # 요청이 들어온 경우 response를 로깅한다. 시각, 어떤 요청에 대한 응답인지, 상태코드를 로깅한다.
+
+        logger.info(f"{datetime.datetime.now()} Request {request.method} {request.url}")
+        # 요청이 들어온 경우 request를 로깅한다. 시각, method, request uri를 로깅한다.
+
+        response = await call_next(request)
+        # 서비스 내에서 request가 처리되고 나면 response가 온다.
+
+        logger.info(f"{datetime.datetime.now()} Response to {request.method} {request.url} Status {response.status_code}")
+        # 요청이 들어온 경우 response를 로깅한다. 시각, 어떤 요청에 대한 응답인지, 상태코드를 로깅한다.
 
         return response
